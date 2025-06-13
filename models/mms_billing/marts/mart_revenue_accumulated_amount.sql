1837455cd8e8a8dc26a0505cac3e9110272f3e5a
Erre <ronalm@cobre.co>
2025-06-13 18:15:45 -0500

    ADD grant

--- models/mms_billing/marts/mart_revenue_accumulated_amount.sql
+++ models/mms_billing/marts/mart_revenue_accumulated_amount.sql
@@ -1,7 +1,7 @@
 {{ config(
     post_hook=[
-        "grant select on view {{ this }} to role data_dev_l1",
-        "grant select on view {{ this }} to role sales_ops_l1"
+        "grant select on view {{ this }} to role DATA_DEV_L1",
+        "grant select on view {{ this }} to role SALES_OPS_DEV_L0"
     ]
 ) }}
 
