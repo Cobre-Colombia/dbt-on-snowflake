98cabe94091e2cc3f33df1cb143bb936e1114c01
Erre <ronalm@cobre.co>
2025-06-13 18:13:59 -0500

    ADD post_hook grant

--- models/mms_billing/marts/mart_revenue_accumulated_amount.sql
+++ models/mms_billing/marts/mart_revenue_accumulated_amount.sql
@@ -1,1 +1,8 @@
+{{ config(
+    post_hook=[
+        "grant select on view {{ this }} to role data_dev_l1",
+        "grant select on view {{ this }} to role sales_ops_l1"
+    ]
+) }}
+
 select * from {{ ref('int_revenue_cumulative_amount') }}
