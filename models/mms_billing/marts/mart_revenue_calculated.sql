c8dc8e696b34eadd96e77a4995866b7a3a2d1aa9
Erre <ronalm@cobre.co>
2025-07-01 14:57:01 -0500

    UPDATE summary

--- models/mms_billing/marts/mart_revenue_calculated.sql
+++ models/mms_billing/marts/mart_revenue_calculated.sql
@@ -14,12 +14,5 @@ from {{ ref('mart_revenue_discount') }}
 union all
 
 select *
-from {{ ref('mart_revenue_true_up_charge') }} tuc
-
-
-{% if is_incremental() %}
-    where local_updated_at > (select max(local_updated_at) from {{ this }})
-{% endif %}
-
-
+from {{ ref('mart_revenue_true_up_charge') }}
 
