6de8b567370fb60e59b151dcdcb5ddfb9c62c621
Erre <ronalm@cobre.co>
2025-07-01 12:24:10 -0500

    update grants

--- models/mms_billing/intermediate/int_rules_mms_count.sql
+++ models/mms_billing/intermediate/int_rules_mms_count.sql
@@ -3,5 +3,5 @@ select
     date_trunc('month', local_created_at) as transaction_month,
     count(*) as trx_count
 from {{ ref('int_rules_mms') }}
-where consumes_saas
+where consumes_saas and should_be_charged
 group by 1, 2
